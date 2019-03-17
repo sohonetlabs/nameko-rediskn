ecc40d6d9396db1efcf5290919ee47f128ff4e57
Julio Trigo <julio.trigo@gmail.com>
2019-03-17 22:25:31 +0000

    Add CI pipeline

---
+++ Makefile
@@ -0,0 +1,44 @@
+.PHONY: test
+
+
+PACKAGE_NAME=nameko_rediskn
+
+RABBIT_CTL_URI?=http://guest:guest@localhost:15672
+AMQP_URI?=amqp://guest:guest@localhost:5672
+
+RABBITMQ_VERSION?=3.6-management
+REDIS_VERSION?=4.0
+
+
+rst-lint:
+	rst-lint README.rst
+	rst-lint CHANGELOG.rst
+
+flake8:
+	flake8 $(PACKAGE_NAME) test
+
+test:
+	pytest test $(ARGS) \
+		--rabbit-ctl-uri $(RABBIT_CTL_URI) \
+		--amqp-uri $(AMQP_URI)
+
+coverage: rst-lint flake8
+	coverage run \
+		--concurrency=eventlet \
+		--source $(PACKAGE_NAME) \
+		-m pytest test $(ARGS) \
+		--rabbit-ctl-uri $(RABBIT_CTL_URI) \
+		--amqp-uri $(AMQP_URI)
+	coverage report --show-missing --fail-under 100
+
+# Docker test containers
+
+docker-rabbitmq-run:
+	docker run -d --rm --name rabbitmq-nameko-rediskn \
+		-p 15672:15672 -p 5672:5672 \
+		rabbitmq:$(RABBITMQ_VERSION)
+
+docker-redis-run:
+	docker run -d --rm --name redis-nameko-rediskn \
+		-p 6379:6379 \
+		redis:$(REDIS_VERSION)
---
+++ setup.py
@@ -0,0 +1,51 @@
+import os
+from codecs import open
+from setuptools import setup, find_packages
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.rst'), 'r', 'utf-8') as handle:
+    readme = handle.read()
+
+setup(
+    name='nameko-rediskn',
+    version='0.1.0',
+    description='Nameko Redis Keyspace Notifications extension.',
+    long_description=readme,
+    long_description_content_type='text/x-rst',
+    author='Julio Trigo',
+    author_email='julio.trigo@sohonet.com',
+    url='https://github.com/sohonetlabs/nameko-rediskn',
+    packages=find_packages(exclude=['test', 'test.*']),
+    install_requires=[
+        'nameko>=2.11',
+        'redis>=2.10.5',
+    ],
+    extras_require={
+        'dev': [
+            'pytest==4.3.1',
+            'flake8',
+            'coverage',
+            'restructuredtext-lint',
+            'Pygments',
+        ],
+    },
+    zip_safe=True,
+    license='MIT License',
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Database",
+        "Topic :: Database :: Front-Ends",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
+)
