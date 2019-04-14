fe7d6a98d948ffbf058dc13f5f3d8bdc9853cc88
Julio Trigo <julio.trigo@gmail.com>
2019-04-14 20:22:46 +0100

    Remove unnecessary rabbit_config fixture

--- test/conftest.py
+++ test/conftest.py
@@ -10,7 +10,7 @@ from test import REDIS_OPTIONS, TIME_SLEEP, URI_CONFIG_KEY
 
 
 @pytest.fixture
-def redis_config():
+def config():
     return {
         'REDIS': {'NOTIFICATION_EVENTS': 'KEA'},
         'REDIS_URIS': {URI_CONFIG_KEY: "redis://localhost:6379/0"},
@@ -18,15 +18,8 @@ def redis_config():
 
 
 @pytest.fixture
-def config(rabbit_config, redis_config):
-    config = rabbit_config.copy()
-    config.update(redis_config)
-    return config
-
-
-@pytest.fixture
-def redis(redis_config):
-    redis_uri = redis_config['REDIS_URIS'][URI_CONFIG_KEY]
+def redis(config):
+    redis_uri = config['REDIS_URIS'][URI_CONFIG_KEY]
     client = StrictRedis.from_url(redis_uri, **REDIS_OPTIONS)
     client.flushall()
     yield client
@@ -34,10 +27,10 @@ def redis(redis_config):
 
 
 @pytest.fixture
-def redis_db_1(redis_config):
+def redis_db_1(config):
     # url argument takes precedence over db in the url
     redis_uri = '{}?db=1'.format(
-        redis_config['REDIS_URIS'][URI_CONFIG_KEY]
+        config['REDIS_URIS'][URI_CONFIG_KEY]
     )
     client = StrictRedis.from_url(redis_uri, db=1, **REDIS_OPTIONS)
     client.flushall()
